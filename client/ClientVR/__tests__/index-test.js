c33509465e3f85bafa4b3fc9f517bea8ce82b360
Dylan Ross Ludwick <dludwick@bimaire.com>
2018-01-19 12:51:39 -0700

    texting fix attempt - revisit

--- client/ClientVR/__tests__/index-test.js
+++ client/ClientVR/__tests__/index-test.js
@@ -1,13 +1,22 @@
 import 'react-native';
 import 'react-vr';
+import {
+  NativeModules
+} from 'react-native';
 import React from 'react';
 import Index from '../index.vr.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+describe('Index', () => {
+  beforeAll(() => {
+    NativeModules.DomOverlayModule = { test: jest.fn() }
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Index />
+    );
+  });
 });
