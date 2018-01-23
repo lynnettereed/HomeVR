8fdcf660627c271ced5c90bdb90f31c2797fff45
Dylan Ross Ludwick <dludwick@bimaire.com>
2018-01-23 15:47:46 -0700

    install and config enzyme, update testing

--- client/ClientVR/__tests__/index-test.js
+++ client/ClientVR/__tests__/index-test.js
@@ -1,23 +1,33 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 import 'react-native';
 import 'react-vr';
-import {
-  NativeModules
-} from 'react-native';
+import { NativeModules } from 'react-native';
+import { shallow } from 'enzyme';
 import React from 'react';
-import Index from '../index.vr.js';
-import DomOverlayModule from '../modules/DomOverlayModule.js';
+import ClientVR from '../index.vr.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-describe('Index', () => {
-  beforeAll(() => {
-    jest.mock('DomOverlayModule', () => 'Test');
-  });
+jest.mock('NativeModules', () => {
+  return {
+    DomOverlayModule: {
+      openPersistent: jest.fn(),
+    },
+  };
+});
+
+describe('<ClientVR />', () => {
 
   it('renders correctly', () => {
-    const tree = renderer.create(
-      <Index />
+    const wrapper = shallow(
+      <ClientVR />
     );
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
