ddae1e59fea4062cf8e8241f85575a0bb65bb1bd
Dylan Ross Ludwick <dludwick@bimaire.com>
2018-02-06 14:57:02 -0700

    integrate scss and gulp

---
+++ client/ClientVR/gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const cssnano = require('cssnano');
+
+// Development mode?
+var devBuild = (process.env.NODE_ENV !== 'production');
+
+// Folders
+var folder = {
+  src: 'scss/',
+  build: 'static_assets/css',
+};
+
+// CSS Processing
+gulp.task('css', function() {
+  var postCssOpts = [
+    autoprefixer({ browsers: ['last 2 versions', '>2%'] }),
+    mqpacker,
+  ];
+
+  if (!devBuild) {
+    postCssOpts.push(cssnano);
+  }
+
+  return gulp.src(folder.src + 'styles-vr.scss')
+    .pipe(sass({
+      outputStyle: 'nested',
+      precision: 3,
+      errLogToConsole: true,
+    }))
+    .pipe(postcss(postCssOpts))
+    .pipe(gulp.dest(folder.build));
+});
