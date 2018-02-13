05fab34ff43d0281f77bee812496ed807439270c
Dylan Ross Ludwick <dludwick@bimaire.com>
2018-02-13 16:08:06 -0700

    add PanoSwitch component

---
+++ client/ClientVR/components/PanoSwitch.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import {
+  asset,
+  StyleSheet,
+  View
+} from 'react-vr';
+
+import PanoLayer from './PanoLayer';
+
+class PanoSwitch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      panoUriA: [],
+      panoUriB: []
+    };
+
+    this.styles = StyleSheet.create({
+      panoLayerA: {
+        display: this.props.showLayerA ? 'flex' : 'none'
+      },
+      panoLayerB: {
+        display: this.props.showLayerB ? 'flex' : 'none'
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.setPanoSource(this.props.updatePhase, this.props.uri);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setPanoSource(nextProps.updatePhase, nextProps.uri);
+  }
+
+  setPanoSource = (updatePhase, uri) => {
+    updatePhase === 'A' ? this.setState({panoUriA: uri}) : this.setState({panoUriB: uri});
+  }
+
+  render() {
+    console.log(`updatePhase: ${this.props.updatePhase}`);
+    console.log(`uri: ${this.props.uri}`);
+    console.log(`panoUriA: ${this.state.panoUriA}`);
+    console.log(`panoUriB: ${this.state.panoUriB}`);
+    console.log(`showLayerA: ${this.props.showLayerA}`);
+    console.log(`showLayerB: ${this.props.showLayerB}`);
+    if (this.state.panoUriA.length > 0 || this.state.panoUriB.length > 0) {
+      return (
+        <View>
+          {this.state.panoUriA.length > 0 ? (
+            <PanoLayer radius={this.props.radius} onLoad={this.props.panoOnLoad}
+                       onLoadEnd={this.props.panoOnLoadEnd}
+                       style={{ display: this.props.showLayerA ? 'flex' : 'none' }}
+                       source={asset(this.state.panoUriA)}
+            />
+          ) : (
+            <View />
+          )}
+          {this.state.panoUriB.length > 0 ? (
+            <PanoLayer radius={this.props.radius} onLoad={this.props.panoOnLoad}
+                       onLoadEnd={this.props.panoOnLoadEnd}
+                       style={{ display: this.props.showLayerB ? 'flex' : 'none' }}
+                       source={asset(this.state.panoUriB)}
+            />
+          ) : (
+            <View />
+          )}
+        </View>
+      );
+    } else {
+      return (
+        <View/>
+      );
+    }
+  }
+}
+
+export default PanoSwitch;
